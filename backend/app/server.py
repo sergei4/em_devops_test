b4da0808fbbd1ccd6d54309012c42abf5dc8f8db
Mikhail E <developer@mobicode.pro>
2026-04-01 15:03:04 +0000

    Setup server configuration

---
+++ backend/app/server.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello from Effective Mobile!</h1>")
+
+HOST = "0.0.0.0"
+PORT = 8080
+
+server = HTTPServer((HOST, PORT), MyHandler)
+print(f"Serving on http://{HOST}:{PORT}")
+server.serve_forever()
